fd2e1b8aad54bb447fffee4e5104332acaa55c1e
salman <alysalman15@gmail.com>
2023-10-09 22:58:29 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var cars = {
+    honda: {
+        civic: {
+            name: "honda civic",
+            year: 2023,
+            price: 5000,
+            colors: ["red", "green", "black"],
+            condition: "new"
+
+        }
+    },
+    toyota: {
+        fortuner: {
+            name: "honda civic",
+            year: 2023,
+            price: 5000,
+            colors: ["red", "green", "black"],
+            condition: "new"
+        },
+        Yaris: {
+            name: "honda civic",
+            year: 2023,
+            price: 5000,
+            colors: ["red", "green", "black"],
+            condition: "new"
+        },
+        Camry: {
+            name: "honda civic",
+            year: 2023,
+            price: 5000,
+            colors: ["red", "green", "black"],
+            condition: "new"
+        },
+        Prius: {
+            name: "honda civic",
+            year: 2023,
+            price: 5000,
+            colors: ["red", "green", "black"],
+            condition: "new"
+        }
+    }
+
+}
