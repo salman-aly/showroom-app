b58ed13693a73de1c97d0c3e103347e504711613
salman <alysalman15@gmail.com>
2023-10-18 20:11:07 +0500

    Images added  everything working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var cars = {
   hyundai: {
     Sonata: {
-      image: "img/cars/hyundai/Sonata.jpg",
+      images: "images/1.jpeg",
       name: "Sonata",
       engine: "Smart Stream 2.5 MPi",
       door: "4 doors",
@@ -11,7 +11,7 @@ var cars = {
       price: "112 Lakh",
     },
     Tucson: {
-      image: "img/cars/hyundai/tucson.webp",
+      images: "images/2.jpeg",
       name: "Tucson",
       engine: "Smart Stream 2.5 MPi",
       door: "4 doors",
@@ -21,7 +21,7 @@ var cars = {
       price: "112 Lakh",
     },
     Accent: {
-      image: "img/cars/hyundai/Accent.avif",
+      images: "images/3.jpeg",
       name: "Accent",
       engine: "Smart Stream 2.5 MPi",
       door: "4 doors",
@@ -31,7 +31,7 @@ var cars = {
       price: "112 Lakh",
     },
     SantaFe: {
-      image: "img/cars/hyundai/SantaFe.png",
+      images: "images/4.jpeg",
       name: "SantaFe",
       engine: "Smart Stream 2.5 MPi",
       door: "4 doors",
@@ -41,136 +41,136 @@ var cars = {
       price: "112 Lakh",
     },
   },
-  // toyota: {
-  //   Camry: {
-  //     image: "img/cars/toyota/Camry.jpg",
-  //     name: "Camry",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   Corolla: {
-  //     image: "img/cars/toyota/Corolla.png",
-  //     name: "Corolla",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
+  toyota: {
+    Camry: {
+      images: "images/5.jpeg",
+      name: "Camry",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Corolla: {
+      images: "images/6.jpeg",
+      name: "Corolla",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
 
-  //   Prius: {
-  //     image: "img/cars/toyota/Prius.webp",
-  //     name: "Prius",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   Fortuner: {
-  //     image: "img/cars/toyota/Fortuner.png",
-  //     name: "Fortuner",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  // },
-  // honda: {
-  //   Civic: {
-  //     image: "img/cars/honda/Civic.jpg",
-  //     name: "Civic",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   City: {
-  //     image: "img/cars/honda/city.jpg",
-  //     name: "City",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   BRV: {
-  //     image: "img/cars/honda/br-v.webp",
-  //     name: "BR-V",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   Hybrid: {
-  //     image: "img/cars/honda/Hybrid.webp",
-  //     name: "CR-V Hybrid",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  // },
-  // suzuki: {
-  //   Swift: {
-  //     image: "img/cars/suzuki/swift.webp",
-  //     name: "Swift",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   Alto: {
-  //     image: "img/cars/suzuki/Alto.jpg",
-  //     name: "Alto",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   Mehran: {
-  //     image: "img/cars/suzuki/Mehran.jpg",
-  //     name: "Mehran",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   Carry: {
-  //     image: "img/cars/suzuki/Carry.jpg",
-  //     name: "Carry",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  // },
+    Prius: {
+      images: "images/7.jpeg",
+      name: "Prius",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Fortuner: {
+      images: "images/8.jpeg",
+      name: "Fortuner",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+  },
+  honda: {
+    Civic: {
+      images: "images/9.jpeg",
+      name: "Civic",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    City: {
+      images: "images/10.jpeg",
+      name: "City",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    BRV: {
+      images: "images/11.jpeg",
+      name: "BR-V",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Hybrid: {
+      images: "images/12.jpeg",
+      name: "CR-V Hybrid",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+  },
+  suzuki: {
+    Swift: {
+      images: "images/13.jpeg",
+      name: "Swift",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Alto: {
+      images: "images/14.jpeg",
+      name: "Alto",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Mehran: {
+      images: "images/15.jpeg",
+      name: "Mehran",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Carry: {
+      images: "images/16.jpeg",
+      name: "Carry",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+  },
   kia: {
     Sportage: {
-      image: "img/cars/kia/Sportage.png",
+      images: "images/17.avif",
       name: "Sportage",
       engine: "Smart Stream 2.5 MPi",
       door: "4 doors",
@@ -180,7 +180,7 @@ var cars = {
       price: "112 Lakh",
     },
     Sorento: {
-      image: "img/cars/kia/Sorento.jpg",
+      images: "images/bmw1.webp",
       name: "Sorento",
       engine: "Smart Stream 2.5 MPi",
       door: "4 doors",
@@ -190,7 +190,7 @@ var cars = {
       price: "112 Lakh",
     },
     Rio: {
-      image: "img/cars/kia/Rio.jpg",
+      images: "images/t1.webp",
       name: "Rio",
       engine: "Smart Stream 2.5 MPi",
       door: "4 doors",
@@ -200,7 +200,7 @@ var cars = {
       price: "112 Lakh",
     },
     Stinger: {
-      image: "img/cars/kia/Stinger.webp",
+      images: "images/1p.jpg",
       name: "Stinger",
       engine: "Smart Stream 2.5 MPi",
       door: "4 doors",
@@ -212,33 +212,30 @@ var cars = {
   },
 };
 
-
-var company = document.getElementById("company")
-var model = document.getElementById("model")
-var allCars = document.getElementById("allCars")
+var company = document.getElementById("company");
+var model = document.getElementById("model");
+var allCars = document.getElementById("allCars");
 
 for (var key in cars) {
   company.innerHTML += `
     <option value="${key}">${key.toUpperCase()}</option>
   `;
-
   for (var key1 in cars[key]) {
-    for (var key2 in cars[key][key1]) {
-      allCars.innerHTML += `
+    var carskey = cars[key][key1]
+    allCars.innerHTML += `
         <div class="row">
           <div class="col">
-            <h3>Description</h3>
-            <h4>${cars[key][key1][key2].name || "unknown"}</h4>
-            <h5>${cars[key][key1][key2].color}</h5>
-            <h5>${cars[key][key1][key2].price}</h5>
+          <div><img src="${carskey.images}"/></div>
+            <h3>${key.toUpperCase()}</h3>
+            <h4>${carskey.name || "unknown"}</h4>
+            <h5>${carskey.color}</h5>
+            <h5>${carskey.price}</h5>
           </div>
         </div>
       `;
-    }
+    console.log(cars[key][key1].images);
   }
 }
-
-
 
 function onChangeCompany() {
   model.innerHTML = "";
@@ -246,24 +243,26 @@ function onChangeCompany() {
     model.innerHTML += `
             <option value="${key}">${key.toUpperCase()}</options>
      `;
-
   }
 }
 
-
-
-
 function filtersCars() {
-  var carData = cars[company.value][model.value].type
-  allCars.innerHTML = `
+  var carData = cars[company.value][model.value];
+
+  var selectedCompany = company.value;
+  var selectedBrand = model.value;
+
+  if (selectedCompany && selectedBrand) {
+    allCars.innerHTML = `
   <div class="row">
     <div class="col">
-      <h3>Description</h3>
+    <div><img src="${carData.images}"/></div>
+      <h3>${carskey[key] || "CarName"}</h3>
       <h4>${carData.name || "unknown"}</h4>
       <h5>${carData.color}</h5>
       <h5>${carData.price}</h5>
     </div>
   </div>
 `;
-
+  }
 }
