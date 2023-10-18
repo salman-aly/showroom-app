0a6d73eebd8e789a1c45b45d4a930b4a40499308
salman <alysalman15@gmail.com>
2023-10-18 11:55:22 +0500

    code push

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
 var cars = {
-  // hyundai: {
-  //   Sonata: {
-  //     image: "img/cars/hyundai/Sonata.jpg",
-  //     name: "Sonata",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   Tucson: {
-  //     image: "img/cars/hyundai/tucson.webp",
-  //     name: "Tucson",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   Accent: {
-  //     image: "img/cars/hyundai/Accent.avif",
-  //     name: "Accent",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  //   SantaFe: {
-  //     image: "img/cars/hyundai/SantaFe.png",
-  //     name: "SantaFe",
-  //     engine: "Smart Stream 2.5 MPi",
-  //     door: "4 doors",
-  //     FuelTank: "60 (Ltr.)",
-  //     MaxPower: "132 (180)/6200",
-  //     color: ["Black", "White", "Red"],
-  //     price: "112 Lakh",
-  //   },
-  // },
+  hyundai: {
+    Sonata: {
+      image: "img/cars/hyundai/Sonata.jpg",
+      name: "Sonata",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Tucson: {
+      image: "img/cars/hyundai/tucson.webp",
+      name: "Tucson",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Accent: {
+      image: "img/cars/hyundai/Accent.avif",
+      name: "Accent",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    SantaFe: {
+      image: "img/cars/hyundai/SantaFe.png",
+      name: "SantaFe",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+  },
   // toyota: {
   //   Camry: {
   //     image: "img/cars/toyota/Camry.jpg",
@@ -62,7 +62,7 @@ var cars = {
   //     color: ["Black", "White", "Red"],
   //     price: "112 Lakh",
   //   },
-  
+
   //   Prius: {
   //     image: "img/cars/toyota/Prius.webp",
   //     name: "Prius",
@@ -179,37 +179,37 @@ var cars = {
       color: ["Black", "White", "Red"],
       price: "112 Lakh",
     },
-    // Sorento: {
-    //   image: "img/cars/kia/Sorento.jpg",
-    //   name: "Sorento",
-    //   engine: "Smart Stream 2.5 MPi",
-    //   door: "4 doors",
-    //   FuelTank: "60 (Ltr.)",
-    //   MaxPower: "132 (180)/6200",
-    //   color: ["Black", "White", "Red"],
-    //   price: "112 Lakh",
-    // },
-    // Rio: {
-    //   image: "img/cars/kia/Rio.jpg",
-    //   name: "Rio",
-    //   engine: "Smart Stream 2.5 MPi",
-    //   door: "4 doors",
-    //   FuelTank: "60 (Ltr.)",
-    //   MaxPower: "132 (180)/6200",
-    //   color: ["Black", "White", "Red"],
-    //   price: "112 Lakh",
-    // },
-    // Stinger: {
-    //   image: "img/cars/kia/Stinger.webp",
-    //   name: "Stinger",
-    //   engine: "Smart Stream 2.5 MPi",
-    //   door: "4 doors",
-    //   FuelTank: "60 (Ltr.)",
-    //   MaxPower: "132 (180)/6200",
-    //   color: ["Black", "White", "Red"],
-    //   price: "112 Lakh",
-    // },
-},
+    Sorento: {
+      image: "img/cars/kia/Sorento.jpg",
+      name: "Sorento",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Rio: {
+      image: "img/cars/kia/Rio.jpg",
+      name: "Rio",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+    Stinger: {
+      image: "img/cars/kia/Stinger.webp",
+      name: "Stinger",
+      engine: "Smart Stream 2.5 MPi",
+      door: "4 doors",
+      FuelTank: "60 (Ltr.)",
+      MaxPower: "132 (180)/6200",
+      color: ["Black", "White", "Red"],
+      price: "112 Lakh",
+    },
+  },
 };
 
 
@@ -250,3 +250,19 @@ function onChangeCompany() {
   }
 }
 
+
+
+function filtersCars() {
+  var carData = cars [company.value][model.value].type
+  allCars.innerHTML = `
+  <div class="row">
+    <div class="col">
+      <h3>Description</h3>
+      <h4>${carData.name || "unknown"}</h4>
+      <h5>${carData.color}</h5>
+      <h5>${carData.price}</h5>
+    </div>
+  </div>
+`;
+
+}
